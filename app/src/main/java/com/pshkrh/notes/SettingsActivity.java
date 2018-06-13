06ebe05f974795ab1d002196969cb5fdf502a930
Pushkar Kurhekar <pshkrh@gmail.com>
2018-06-13 10:03:00 +0530

    Remove unused imports from Settings

--- app/src/main/java/com/pshkrh/notes/SettingsActivity.java
+++ app/src/main/java/com/pshkrh/notes/SettingsActivity.java
@@ -1,35 +1,10 @@
 package com.pshkrh.notes;
 
-import android.annotation.TargetApi;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.content.res.Configuration;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.EditTextPreference;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompat;
-
-import java.util.List;
 
 public class SettingsActivity extends AppCompatPreferenceActivity {
     private static final String TAG = SettingsActivity.class.getSimpleName();
